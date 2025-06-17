6d864ec79caba3fdcdd010a5de9278b81fd1ba66
Johnny Diaz <johnnydiaz@Johnnys-MacBook-Pro.local>
2025-06-17 13:57:09 -0700

    learning pushing

---
+++ lab2.py
@@ -0,0 +1,11 @@
+name = "Johnny Diaz"
+
+nickname= 'johnny'
+
+age= "17"
+
+used_python = True
+
+hobbies = ["gaming", "calling friends", "watching movies", "hanging out with friends"]
+
+Favorite_things = {"dream pet":"ferret", "insect":"Moths and Ferret", "genera for shows":"anime", "color":"yellow" }
